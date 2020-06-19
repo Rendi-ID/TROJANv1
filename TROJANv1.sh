8193c6c2371c1c41f328a9771ab3b046a3833a7e
Randi-ID <66753543+Rendi-ID@users.noreply.github.com>
2020-06-19 14:32:25 +0700

    Add files via upload

---
+++ TROJANv1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear
+read -p "Siapa Nama Ente : " nama
+echo "Hai Tuan $nama Selamat Datang Di Tools Ane"
+sleep 2
+echo "Sebelum Menggunakan Tools ini"
+sleep 2
+echo "Alangkah Baiknya Kita Berantem Dulu"
+sleep 2
+echo "Knapa?Ente Takut?...Lemah"
+sleep 2
+clear
+war="Gunakan Tools Ini Dengan Bijak!"
+for i in {1..47}; do
+	echo -ne "\r${war:0:$i}";
+        sleep 0.1
+done
+sleep 2
+clear
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬" | lolcat
+echo "[•] Author : Randiansyah" | lolcat
+echo "[•] Youtube: Rendy Noober" | lolcat
+echo "[•] Github : github.com/Rendi-ID" | lolcat
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬" | lolcat
+read -p "Masukan nomor target : " nomor 
+while [[ true ]]; do
+echo "mencoba mengirim virus ke nomor $nomor"
+done
